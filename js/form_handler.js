ec9ba255d23fca3509da5fdc8c556aa7927709fe
Аkril0 <alexbatin2003@gmail.com>
2024-03-19 14:03:22 +0200

    Fix Errors

--- js/form_handler.js
+++ js/form_handler.js
@@ -3,7 +3,7 @@ const userName = document.getElementById('userName');
 const phoneNumber = document.getElementById('phoneNumber');
 const color = document.getElementById('color');
 const size = document.getElementById('size');
-
+const model = document.getElementById('model');
 phoneNumber.oninput = function(){
     this.value = this.value.replace(/[^0-9\+]/g, '');
 }
@@ -11,7 +11,7 @@ phoneNumber.oninput = function(){
 const sendForm = async ()=>{
     try{
 
-        await fetch('/',{
+        await fetch('https://modnapearl.shop/bot/',{
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
@@ -22,15 +22,19 @@ const sendForm = async ()=>{
                 [phoneNumber.name]: phoneNumber.value.trim(),
                 [color.name]: color.value.trim(),
                 [size.name]:size.value,
+		        [model.name]:model.value,
             })
         }).then(response =>{
                 if (response.status >= 200 && response.status < 300){
                     form.reset();
+		            alert("Ми отримали замовлення та з'вяжимося з вами найближчим часом!")
                 }else{
-                    alert('Ошибка');
+                    alert('Помилка');
                 }
             }
-        );
+        ).catch(()=>{
+            alert('Помилка');
+        });
     }catch(ex){
         console.error(ex);
     }
@@ -39,12 +43,23 @@ const sendForm = async ()=>{
 
 form.onsubmit = (event) => {
         event.preventDefault();
-        let nameVal = userName.value.trim();
         let phoneVal = phoneNumber.value.trim();
         let colorVal = color.value;
         let sizeVal = size.value;
 
-        console.log(nameVal, phoneVal, colorVal, sizeVal)
+        if (colorVal === "Оберіть колір"){
+            alert("Оберіть колір");
+            return false;
+        }
+        if (sizeVal === "Оберіть розмір"){
+            alert("Оберіть розмір");
+            return false;
+        }
+        if (phoneVal.indexOf('_') !== -1 || phoneVal == null || typeof phoneVal === 'undefined' || phoneVal == '') {
+            alert('Введіть ваш номер телефона!');
+            return false;
+        }
+        sendForm();
 
     return false;
 }
